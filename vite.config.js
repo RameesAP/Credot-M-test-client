415f29270b788149104eedfcb4d9d6b8a9eb229f
RameesAP <rameeswork@gmail.com>
2024-02-19 01:07:48 +0530

    uu

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   server: {
-    // proxy: {
-    //   "/api": {
-    //     target: "https://credotbackramees.onrender.com",
-    //     secure: true,
-    //     changeOrigin: true,
-    //     credentials: "include",
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: "https://credotbackramees.onrender.com",
+        secure: false,
+
+      },
+    },
     port: 3000,
   },
   plugins: [react()],
